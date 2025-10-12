92b6d9ea7908c17e9921c91918be4f8dd2babd86
meftunca <buraksenturk25@gmail.com>
2025-10-12 21:23:34 +0300

    Sync

--- examples/assembly-poc/main.go
+++ examples/assembly-poc/main.go
@@ -1,8 +1,7 @@
 package main
+
 // Assembly Optimization Proof-of-Concept Example
 // This demonstrates how WriteCompressedUint could be implemented in Assembly
-
-package main
 
 import (
 	"fmt"
@@ -15,20 +14,20 @@ func writeCompressedUintGo(n uint64, buf []byte) int {
 		buf[0] = byte(n << 2)
 		return 1
 	}
-	
+
 	if n < 16384 {
 		buf[0] = byte(0x01 | ((n >> 8) << 2))
 		buf[1] = byte(n)
 		return 2
 	}
-	
+
 	if n < 1073741824 {
 		buf[0] = byte(0x02 | ((n >> 16) << 2))
 		buf[1] = byte(n >> 8)
 		buf[2] = byte(n)
 		return 3
 	}
-	
+
 	buf[0] = byte(0x03 | ((n >> 24) << 2))
 	buf[1] = byte(n >> 16)
 	buf[2] = byte(n >> 8)
@@ -43,14 +42,14 @@ TEXT ·writeCompressedUintAsm(SB), NOSPLIT, $0-32
     // Arguments:
     //   n uint64    @ 8(SP)
     //   buf []byte  @ 16(SP)
-    
+
     MOVQ    n+0(FP), AX        // Load n into AX
     MOVQ    buf+8(FP), DI      // Load buf pointer into DI
-    
+
     // Fast path: n < 64 (no branches with CMOV)
     CMPQ    AX, $64
     JGE     two_byte
-    
+
 one_byte:
     SHLQ    $2, AX             // n << 2
     MOVB    AL, 0(DI)          // buf[0] = byte(n << 2)
@@ -60,7 +59,7 @@ one_byte:
 two_byte:
     CMPQ    AX, $16384
     JGE     three_byte
-    
+
     MOVQ    AX, BX             // Copy n
     SHRQ    $8, BX             // n >> 8
     SHLQ    $2, BX             // (n >> 8) << 2
@@ -73,7 +72,7 @@ two_byte:
 three_byte:
     CMPQ    AX, $1073741824
     JGE     four_byte
-    
+
     MOVQ    AX, BX
     SHRQ    $16, BX
     SHLQ    $2, BX
@@ -108,7 +107,7 @@ func BenchmarkWriteCompressedUint_Go_Small(b *testing.B) {
 	buf := make([]byte, 8)
 	b.ResetTimer()
 	b.ReportAllocs()
-	
+
 	for i := 0; i < b.N; i++ {
 		_ = writeCompressedUintGo(uint64(i%64), buf)
 	}
@@ -118,7 +117,7 @@ func BenchmarkWriteCompressedUint_Go_Medium(b *testing.B) {
 	buf := make([]byte, 8)
 	b.ResetTimer()
 	b.ReportAllocs()
-	
+
 	for i := 0; i < b.N; i++ {
 		_ = writeCompressedUintGo(uint64(i%16384), buf)
 	}
@@ -128,7 +127,7 @@ func BenchmarkWriteCompressedUint_Go_Large(b *testing.B) {
 	buf := make([]byte, 8)
 	b.ResetTimer()
 	b.ReportAllocs()
-	
+
 	for i := 0; i < b.N; i++ {
 		_ = writeCompressedUintGo(uint64(i), buf)
 	}
@@ -158,9 +157,9 @@ func BenchmarkWriteCompressedUint_Go_Large(b *testing.B) {
 
 func main() {
 	buf := make([]byte, 8)
-	
+
 	testCases := []uint64{0, 10, 63, 64, 100, 16383, 16384, 1073741823, 1073741824, 4294967295}
-	
+
 	for _, n := range testCases {
 		len := writeCompressedUintGo(n, buf)
 		fmt.Printf("n=%d → %d bytes: %v\n", n, len, buf[:len])
